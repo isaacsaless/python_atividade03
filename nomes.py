60b1a946298c8976c9ec343297d4a4b98f6724a9
isaacsaless <isaacnascimentosales@gmail.com>
2023-10-11 10:29:44 -0300

    Primeira versão do código de mostrar nomes

---
+++ nomes.py
@@ -0,0 +1,10 @@
+import time
+nome1 = str(input("Digite o nome 1:"))
+nome2 = str(input("Digite o nome 2:"))
+nome3 = str(input("Digite o nome 3:"))
+time.sleep (0.7)
+print(nome1)
+time.sleep (5.0)
+print(nome2)
+time.sleep (5.0)
+print(nome3)
